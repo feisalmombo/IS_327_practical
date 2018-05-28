b635f64e37bcb4a75347009b280156a423c916e1
Msangi21 <alaminhamadi@gmail.com>
2018-05-28 18:09:37 +0300

    I just create a first session. Which show the menu, So Feisal create a session for service

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,155 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
+
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      
+        <meta name="description" content="">
+        <meta name="author" content="">
+      
+        <title>Natma Tech</title>
+      
+        <!-- Bootstrap core CSS -->
+      
+        <link href="../css/vendors/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="../css/full-slider.css">
+      
+      
+      
+        <!-- Custom fonts for this template -->
+        <link href="../css/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+      
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+        <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+      
+        <!-- Custom styles for this template -->
+        <link href="../css/agency.min.css" rel="stylesheet">
+      
+      </head>
+    <body id="page-top">
+        <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
 
-        <title>Laravel</title>
+    <a class="navbar-brand" style="font-family:Helvetica;" href="#page-top">
+      <img src="../imgs/logo.png" class="img-responsive" style="height: 60px; width: 70px;">
+      Natma Tech Company LTD</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fa fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#portfolio">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#services">Products</a>
+          </li>
+          
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#team">Team</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    </nav>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+    <header>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner" role="listbox">
+            <!-- Slide One - Set the background image for this slide in the line below -->
+            <div class="carousel-item active" style="background-image: url('../imgs/cable12.jpg')">
+              <div class="carousel-caption d-none d-md-block">
+                <h3>Router & Switch</h3>
+                {{-- <p>This is a description for the first slide.</p> --}}
+              </div>
             </div>
+  
+  
+            <!-- Slide Two - Set the background image for this slide in the line below -->
+            <div class="carousel-item" style="background-image: url('../imgs/header4.jpg')">
+              <div class="carousel-caption d-none d-md-block">
+                <h3>Network Cables</h3>
+                <p></p>
+              </div>
+            </div>
+            <!-- Slide Three - Set the background image for this slide in the line below -->
+            <div class="carousel-item" style="background-image: url('../imgs/design.png')">
+              <div class="carousel-caption d-none d-md-block">
+                <h3>Website Design</h3>
+                <p></p>
+              </div>
+            </div>
+  
+            <!-- Slide Four - Set the background image for this slide in the line below -->
+            <div class="carousel-item" style="background-image: url('../imgs/image5.png')">
+              <div class="carousel-caption d-none d-md-block">
+                <h3>Web Securit</h3>
+                <p></p>
+              </div>
+            </div>
+            <!-- Slide Four - Set the background image for this slide in the line below -->
+            <div class="carousel-item" style="background-image: url('../imgs/header8.jpg')">
+              <div class="carousel-caption d-none d-md-block">
+                <h3>Web Designing</h3>
+                <p></p>
+              </div>
+            </div>
+  
+            <!-- Slide Four - Set the background image for this slide in the line below -->
+            <div class="carousel-item" style="background-image: url('../imgs/all.png')">
+              <div class="carousel-caption d-none d-md-block">
+                <h3>Our Services</h3>
+                <p></p>
+              </div>
+            </div>
+  
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
+      </header>
+
+
+      <!-- Bootstrap core JavaScript -->
+          <script src="../css/vendor/jquery/jquery.min.js"></script>
+          <script src="../css/vendor/popper/popper.min.js"></script>
+          <script src="../css/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+          <!-- Plugin JavaScript -->
+          <script src="../css/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+          <!-- Contact form JavaScript -->
+          <script src="../js/jqBootstrapValidation.js"></script>
+          <script src="../js/contact_me.js"></script>
+
+          <!-- Custom scripts for this template -->
+          <script src="../js/agency.min.js"></script>
+
+          <!-- Bootstrap core JavaScript -->
+          <script src="../css/vendors/jquery/jquery.min.js"></script>
+          <script src="../css/vendors/popper/popper.min.js"></script>
+          <script src="../css/vendors/bootstrap/js/bootstrap.min.js"></script>
     </body>
 </html>
