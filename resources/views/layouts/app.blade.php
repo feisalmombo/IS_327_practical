b4f7f42d93e09e976df0d04b1986efaece41ad53
feisal mombo <feisalmombo29@gmail.com>
2018-06-14 21:51:19 +0300

    modified all files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="icon" href="/favicon.ico">
+
     <title>High Hub-@yield('title') </title>
 
     <!-- Styles -->
